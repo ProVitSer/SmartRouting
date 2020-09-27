572d5bb6ec4db4ae4b1cfda42a10742c43495d56
Vitaliy Prokin <vitaliyprokin@MacBook-Air-Vitaliy.local>
2020-09-27 16:43:37 +0300

    add send 3cx/module id info to 1c. Refactoring

--- send.js
+++ send.js
@@ -1,13 +1,14 @@
 "use strict";
 const Soap = require('./soap'),
-       logger = require('./logger'),
-       util = require('util'),
-       url = require(`url`),
-       moment = require('moment'),
-       bodyParser = require('body-parser'),
-       express = require('express'),
-       port = 3000,
-       app = express();
+    logger = require('./logger/logger'),
+    util = require('util'),
+    url = require(`url`),
+    moment = require('moment'),
+    bodyParser = require('body-parser'),
+    db = require('./db'),
+    express = require('express'),
+    port = 3000,
+    app = express();
 
 const soap = new Soap();
 
@@ -16,28 +17,49 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-app.get('/rest*',(req, res) => {
+app.get('/sendNotWorkinTimeInfo*', (req, res) => {
     let queryData = url.parse(req.url, true).query;
     res.sendStatus(200);
     let timestamp = moment().format("YYYY-MM-DDTHH:mm:ss");
     let dialExtension = queryData.dialExtension > 8 ? queryData.dialExtension : "8495" + queryData.dialExtension;
     let incomingNumber
-    if (queryData.incomingNumber.length == 10){
-    	incomingNumber = '8' + queryData.incomingNumber
+    if (queryData.incomingNumber.length == 10) {
+        incomingNumber = '8' + queryData.incomingNumber
     } else {
-    	incomingNumber = queryData.incomingNumber
+        incomingNumber = queryData.incomingNumber
     }
     soap.getNumber(incomingNumber, dialExtension, timestamp)
-    .then(
-    	result => {
-          console.log(result)
-    	}
-    )
-    .catch(error => {
-    	console.log(error)
-    });
+        .then(
+            result => {
+                console.log(result)
+            }
+        )
+        .catch(error => {
+            console.log(error)
+        });
 });
 
+app.get('/send3cxIdModelId*', (req, res) => {
+    let queryData = url.parse(req.url, true).query;
+    res.sendStatus(200);
+    console.log(queryData);
+    search3cxId(queryData.incomingNumber.slice(1), queryData.unicueid);
+});
+
+
+const search3cxId = (incomingNumber, unicueid) => {
+    db.any(`SELECT call_id FROM cl_participants WHERE info_id = (SELECT id FROM cl_party_info WHERE caller_number like '%${incomingNumber}' ORDER BY id LIMIT 1);`)
+        .then(
+            queue => {
+                console.log(queue);
+                console.log(queue[0].call_id, unicueid)
+                soap.sendInfoAfterHangup(unicueid, queue[0].call_id);
+            }
+        )
+        .catch(error => {
+            console.log(util.inspect(error));
+        });
+};
 
 const server = app.listen(port, (error) => {
     if (error) return logger.error(`Error: ${error}`);
--- soap.js
+++ soap.js
@@ -1,8 +1,8 @@
 'use strict';
 const axios = require('axios'),
     util = require('util'),
-    logger = require(`./logger`),
-    config = require(`./config`);
+    logger = require(`./logger/logger`),
+    config = require(`./config/config`);
 
 class Soap {
     constructor(username = config.webServer.username, password = config.webServer.secret, url = config.webServer.url) {
@@ -15,16 +15,17 @@ class Soap {
         logger.info(`getNumber данные  ${params}`);
         let auth = "Basic " + new Buffer(`${this.username}:${this.password}`).toString("base64");
 
-	//params[0] - Входящий номер, params[1] - номер куда звонят, params[2] время поступления входящего вызова
-	//Парсинг ответа ['soap:Envelope']['soap:Body']['m:ReturnNumberResponse']['m:return']['_text']
-	params[1] = params[1].length > 8 ?  params[1] : "8495" + params[1];
+        //params[0] - Входящий номер, params[1] - номер куда звонят, params[2] время поступления входящего вызова
+        //Парсинг ответа ['soap:Envelope']['soap:Body']['m:ReturnNumberResponse']['m:return']['_text']
+        params[1] = params[1].length > 8 ? params[1] : "8495" + params[1];
         let xml = `<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ret="ReturnNumber">
 		<soap:Header/>
 		  <soap:Body>
 		   <ret:ReturnNumber>
 		      <ret:Number>${params[0]}</ret:Number>
 		      <ret:Number1>${params[1]}</ret:Number1>
-                      <ret:DateTime>${params[2]}</ret:DateTime>
+              <ret:DateTime>${params[2]}</ret:DateTime>
+              <ret:ID>${params[3]}</ret:ID>
 		   </ret:ReturnNumber>
 		  </soap:Body>
 		</soap:Envelope>`
@@ -39,17 +40,53 @@ class Soap {
             }
         }
 
-        logger.info(`Отправляем запрос ${xml}`);
+        logger.info(`Отправляем запрос на получении добавочного для даннх ${xml}`);
         const res = await axios.post(this.url, xml, config)
         const result = await res;
 
-        logger.info(`Получили результат на запрос ${result.data}`);
+        logger.info(`Получили результат на запрос получения добавочного ${result.data}`);
         if (!result) {
             logger.error('Отсутствует результат');
             return [];
         }
         return result.data;
     };
+
+    async sendInfoAfterHangup(...params) {
+        logger.info(`sendInfoAfterHangup данные  ${params}`);
+        let auth = "Basic " + new Buffer(`${this.username}:${this.password}`).toString("base64");
+
+        let xml = `<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ret="ReturnNumber">
+                <soap:Header/>
+                  <soap:Body>
+                   <ret:SetID>
+                      <ret:ID>${params[0]}</ret:ID>
+                      <ret:ID3CX>${params[1]}</ret:ID3CX>
+                   </ret:SetID>
+                  </soap:Body>
+                </soap:Envelope>`
+
+
+        let config = {
+            headers: {
+                'User-Agent': 'icepartners/0.0.1',
+                'Content-Type': 'application/soap+xml;charset=utf-8',
+                'Content-Length': xml.length,
+                //'Authorization': auth
+            }
+        }
+        logger.info(`Отправляем информацию с данными 3cx\module id ${xml}`);
+        const res = await axios.post(this.url, xml, config)
+        const result = await res;
+
+        logger.info(`Получили результат отправленную информацию ${result.data}`);
+        if (!result) {
+            logger.error('Отсутствует результат');
+            return [];
+        }
+        return result.data;
+    };
+
 };
 
 module.exports = Soap;
